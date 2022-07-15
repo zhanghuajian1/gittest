dea1ecdf1fa34de7ce00687f35621bd0ffe75ce3
zhanghuajian1 <zhanghuajian@xiaomi.com>
2022-07-15 15:49:43 +0800

    First Commit

---
+++ src/com/mi/Mi.java
@@ -0,0 +1,16 @@
+package com.mi;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Mi {
+    public static void main(String[] args) {
+        List list = new ArrayList<Integer>();
+        int[] da = new int[10];
+        Map<Integer, Integer> integerIntegerMap = new HashMap<Integer, Integer>();
+        list.add(1);
+        System.out.println(list.toString());
+    }
+}
